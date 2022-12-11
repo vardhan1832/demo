a526e1ba3f267c71cb1130cd2b021d1c1416db61
vardhan1831 <sreevardhan259@gmail.com>
2022-12-11 20:15:04 +0530

    let items=document.getElementsByClassName('list-group-item'); items[2].style.backgroundColor='green'; for(let i=0;i<items.length;i++){     items[i].style.fontWeight='1000'; }

--- new.js
+++ new.js
@@ -3,3 +3,8 @@ let item = document.getElementById('span-item');
 item.style.fontWeight= '1000';
 item.style.color='green';
 
+let items=document.getElementsByClassName('list-group-item');
+items[2].style.backgroundColor='green';
+for(let i=0;i<items.length;i++){
+    items[i].style.fontWeight='1000';
+}
