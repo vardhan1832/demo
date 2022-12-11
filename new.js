e124adf64129599e5a7222f7cbce500bd69dcf74
vardhan1831 <sreevardhan259@gmail.com>
2022-12-11 20:44:29 +0530

    tag name

--- new.js
+++ new.js
@@ -8,3 +8,8 @@ items[2].style.backgroundColor='green';
 for(let i=0;i<items.length;i++){
     items[i].style.fontWeight='1000';
 }
+
+let li = document.getElementsByTagName('li');
+for(let i=0;i<li.length;i++){
+    li[i].style.fontWeight='1000';
+}
