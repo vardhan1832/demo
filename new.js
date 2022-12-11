ae45f12d2205a957533380311a58e89958231fd6
vardhan1831 <sreevardhan259@gmail.com>
2022-12-11 16:46:33 +0530

    addItems

---
+++ new.js
@@ -0,0 +1,5 @@
+document.getElementById('header-title').style.borderBottom='solid 4px #000';
+let item = document.getElementById('span-item');
+item.style.fontWeight= '1000';
+item.style.color='green';
+
