c746f22a4657b8a195d836a3c7ff66d561860710
vardhan1831 <sreevardhan259@gmail.com>
2023-05-03 17:20:54 +0530

    url-page

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
 const http = require('http');
 const server = http.createServer((req,res)=>{
-    console.log('vardhan');
+    const url = req.url;
+    if(url === '/home'){
+        res.write('<html>')
+        res.write('<body><h1>Welcome Home</h1></body>')
+        res.write('</html>')
+        return res.end();
+    }else if(url === '/about'){
+        res.write('<html>')
+        res.write('<body><h1>Welcome to About us page </h1></body>')
+        res.write('</html>')
+        return res.end();
+    }else if(url === '/node'){
+        res.write('<html>')
+        res.write('<body><h1>Welcome to my Node.js project!!</h1></body>')
+        res.write('</html>')
+        return res.end();
+    }
 })
 server.listen(4000);
