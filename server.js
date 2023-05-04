6bd0b8911a709521a4fb1fefce367508aaae49c9
vardhan1831 <sreevardhan259@gmail.com>
2023-05-04 16:19:18 +0530

    clean_code.server

--- server.js
+++ server.js
@@ -1,30 +1,9 @@
 const http = require('http');
-const fs = require('fs');
-let message = '';
-const server = http.createServer((req,res)=>{
-    const url = req.url;
-    const method = req.method;
-    
-    if(url === '/'){
-        res.write('<html>')
-        res.write(`<body>${message}<br><form action="/message" method="POST"><input type="text" name="message"><button>submit</button></form></body>`)
-        res.write('</html>')
-        return res.end();
-    } 
-    if(url === '/message' && method === 'POST'){
-        const body = [];
-        req.on('data',(chunk)=>{
-            body.push(chunk);
-        })
-        req.on('end',()=>{
-            const parsedBody = Buffer.concat(body).toString();
-            message = parsedBody.split('=')[1];      
-            fs.writeFile('message.txt', message , nxt=>{
-                res.statusCode = 302;
-                res.setHeader('location', '/');
-                return res.end();
-            });
-        })     
-    }
-})
+
+const routes = require('./routes')
+ 
+console.log(routes.text);
+
+const server = http.createServer(routes.handler);
+
 server.listen(4000);
