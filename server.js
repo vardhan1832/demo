409c3fc8f558988c06fd1fe6a957f0d42ba29331
vardhan1831 <sreevardhan259@gmail.com>
2023-05-05 19:44:51 +0530

    Express-basics

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-const http = require('http');
+const express = require('express');
+const server = express();
 
-const routes = require('./routes')
- 
-console.log(routes.text);
-
-const server = http.createServer(routes.handler);
+server.use((req,res,next)=>{
+    res.send("<h1>'Hello Express'<h1>");
+    //res.send({text: '111'})
+    next();
+})
+server.use((req,res,next)=>{
+    console.log('hiiii')
+})
 
 server.listen(4001);
