1d4f60696f34554eae597413b3cef300a11939e7
vardhan1831 <sreevardhan259@gmail.com>
2023-05-05 23:59:44 +0530

    bodyParser

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const server = express();
 
-server.use((req,res,next)=>{
-    res.send("<h1>'Hello Express'<h1>");
-    //res.send({text: '111'})
-    next();
+const bodyParser = require('body-parser')
+
+server.use(bodyParser.urlencoded({extended: false}));
+
+server.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add product</button></form>');
+  
 })
-server.use((req,res,next)=>{
-    console.log('hiiii')
+server.post('/product',(req,res,next)=>{
+    console.log(req.body.title)
+    console.log(req.body.size)
+    res.redirect('/')
+})
+server.use('/',(req,res,next)=>{
+    res.send('<h1>Home page</h1>')
 })
 server.listen(4001);
