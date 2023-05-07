483e16b16d2eed7c026b7fed736d8545616ded94
vardhan1831 <sreevardhan259@gmail.com>
2023-05-07 13:52:12 +0530

    html-css-js-serverPages

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path')
+const rootDir = require('../util/path')
 
 router.get('/add-product',(req,res,next)=>{
-    res.send('<form action="/admin/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add product</button></form>');
+    res.sendFile(path.join(rootDir,'views','addprod.html'));
   
 })
 router.post('/product',(req,res,next)=>{
--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 const express = require('express');
 const server = express();
+const path = require('path');
 
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
+const contactRoutes = require('./routes/contact')
 
 const bodyParser = require('body-parser')
 
 server.use(bodyParser.urlencoded({extended: false}));
+server.use(express.static(path.join(__dirname,'public')))
 
 
 server.use('/admin',adminRoutes);
 server.use('/shop',shopRoutes);
+server.use(contactRoutes);
 
 server.use((req,res,next)=>{
-    res.status(404).send('<h1>Page not found</h1>');
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 })
 
 
