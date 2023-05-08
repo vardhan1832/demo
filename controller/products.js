b2a3d26442f7b1689bd97de662a9b90429309e8f
vardhan1831 <sreevardhan259@gmail.com>
2023-05-08 18:50:37 +0530

    models

--- controller/products.js
+++ controller/products.js
@@ -1,14 +1,18 @@
 const path = require('path')
 const rootDir = require('../util/path')
+const Product = require('../models/product')
 
 exports.getAddProducts = (req,res,next)=>{
     res.sendFile(path.join(rootDir,'views','addprod.html'));
 }
 exports.postAddProducts = (req,res,next)=>{
-    console.log(req.body.title)
-    console.log(req.body.size)
+    const product = new Product(req.body.title, req.body.size);
+    product.save();
     res.redirect('/shop')
 }
 exports.getShop = (req,res,next)=>{
+    Product.fetchall((pro)=>{
+        console.log(pro);
+    });
     res.sendFile(path.join(rootDir,'views','shop.html'))
 }
---
+++ models/product.js
@@ -0,0 +1,31 @@
+const fs= require('fs')
+const path = require('path')
+const rootDir = require('../util/path')
+const p = path.join(rootDir,'data' , 'products.json')
+
+const readProductsFromFile = (cb)=>{
+    fs.readFile(p,(err,filecontect)=>{
+        if(err){
+          cb([])
+        }else{
+            cb(JSON.parse(filecontect));
+        }   
+    })
+}
+module.exports=class Product{
+    constructor(t,a){
+        this.title = t;
+        this.size = a;
+    }
+    save(){
+      readProductsFromFile(arr=>{
+            arr.push(this);
+            fs.writeFile(p,JSON.stringify(arr),err=>{
+                console.log(err);
+            });
+      });
+    }
+    static fetchall(cb){
+        readProductsFromFile(cb);
+    }
+}
